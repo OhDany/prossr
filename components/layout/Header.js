5fcb0192145a19b9e399029ec4e6d464247dbcf4
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-11 17:14:33 -0500

    Creating Header Component

---
+++ components/layout/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Buscar from '../ui/buscar';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <p>P</p>
+
+          <Buscar />
+
+          {/* Nav Aqui */}
+        </div>
+
+        <div>
+          {/* Menu de Administración */}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ pages/nosotros.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/layout/Layout';
+
+const Nosotros = () => {
+	return (
+		<div className="container">
+			<Layout />
+			<h1>Nosotros</h1>
+		</div>
+	);
+};
+
+export default Nosotros;
