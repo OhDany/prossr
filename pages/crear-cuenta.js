1cb5008817373524ea64d76a0768dd019bded64c
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-12 10:22:22 -0500

    Add css to Form Signup

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,43 +1,50 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario';
 
 const CrearCuenta = () => (
   <div>
     <Layout>
       <>
-      <h1>Crear Cuenta</h1>
-      <form>
-        <div>
+      <h1
+        css = {css`
+          text-align: center;
+          margin-top: 5rem;
+        `}
+      >Crear Cuenta</h1>
+      <Formulario>
+        <Campo>
           <label htmlFor="nombre">Nombre</label>
           <input
             type="text"
             id="nombre"
             placeholder="Tu Nombre"
           />
-        </div>
+        </Campo>
 
-        <div>
+        <Campo>
           <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
             placeholder="Email"
           />
-        </div>
+        </Campo>
 
-        <div>
+        <Campo>
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             placeholder="Password"
           />
-        </div>
+        </Campo>
 
-        <input type="submit"
+        <InputSubmit type="submit"
           value="Crear Cuenta"
         />
-      </form>
+      </Formulario>
       </>
     </Layout>
   </div>
