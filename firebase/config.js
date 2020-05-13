ea2ca7bf43e196c7857d7231922baadc7efb0a1d
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-13 09:20:51 -0500

    Config Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSP-vChOyJ3IVSt_3pjRN5lwiDHN9XG7w",
+  authDomain: "product-hunt-df444.firebaseapp.com",
+  databaseURL: "https://product-hunt-df444.firebaseio.com",
+  projectId: "product-hunt-df444",
+  storageBucket: "product-hunt-df444.appspot.com",
+  messagingSenderId: "458037358411",
+  appId: "1:458037358411:web:f3926c5d64fb584c5a02ce"
+};
+
+export default firebaseConfig;
