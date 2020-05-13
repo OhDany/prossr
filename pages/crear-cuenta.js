06059941a33945513e39d076f9a0c72a836fe715
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-13 10:27:28 -0500

    Create method of Register User

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -3,6 +3,9 @@ import { css } from '@emotion/core';
 import Layout from '../components/layout/Layout';
 import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
+// Firebase index
+import firebase from '../firebase'
+
 // Validaciones
 import useValidacion from '../hooks/useValidacion';
 import validarCrearCuenta from '../validacion/validarCrearCuenta';
@@ -19,8 +22,12 @@ const CrearCuenta = () => {
 
   const { nombre, email, password } = valores;
 
-  function crearCuenta() {
-    console.log('Creando cuenta');
+  async function crearCuenta() {
+    try {
+      await firebase.registrar(nombre, email, password);
+    } catch (error) {
+      console.error('Hubo un error al crear el usuario', error.message);
+    }
   }
 
   return (
