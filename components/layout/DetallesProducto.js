b942063a227ea911de1c4f93971b7d56572f6e08
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-13 19:34:52 -0500

    Show Database Information

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale';
+
+const Imagen = styled.img`
+  width: 200px;
+`;
 
 const DetallesProducto = ({producto}) => {
 
-  const { id, comentarios, creado, descripcion, empresa, nombre, url, urlimagen, votos } = producto;
+  const { id, comentarios, creado, descripcion, empresa, nombre, url, urlImagen, votos } = producto;
 
   return (
     <li>
       <div>
         <div>
-
+          <Imagen src={urlImagen} />
         </div>
         <div>
           <h1>{nombre}</h1>
+          <p>{descripcion}</p>
+          <div>
+            <img src="/static/img/comentario.png" />
+            <p>{comentarios.length} Comentarios</p>
+          </div>
+
+          <p>Publicado hace: { formatDistanceToNow(new Date(creado), {locale:es} )}</p>
         </div>
       </div>
 
       <div>
-
+        <di>&#9650;</di>
+        <div>{votos}</div>
       </div>
     </li>
   );
