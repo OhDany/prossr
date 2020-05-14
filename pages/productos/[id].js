e2a5bed064d2324ce924129e1bb38e3ee04a1db6
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-14 17:21:31 -0500

    Dinaic Routing 2

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const Producto = () => {
-  return ( <h1>Desde [id].js</h1> );
+const Producto = (props) => {
+  // Routing para obtener el ID actual
+  const router = useRouter();
+  const { query: { id }} = router
+
+  useEffect(() => {
+    if (id) {
+      console.log('Ya hay un id', id);
+    }
+  }, [id])
+
+  return ( <h1>Desde {id}</h1> );
 }
  
 export default Producto;
