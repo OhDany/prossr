a9f15fbb1f7965365198ba700eb779bc6832e2cb
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-16 08:50:33 -0500

    Show Productos for Votos

--- pages/populares.js
+++ pages/populares.js
@@ -1,12 +1,33 @@
+  
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
-const Populares = () => (
-  <div>
-    <Layout>
-      <h1>Populares</h1>
-    </Layout>
-  </div>
-)
+
+const Populares = () => {
+
+  const { productos } = useProductos('votos');
+
+	return (
+		<div className="container">
+			<Layout>
+				<div className="listado-productos">
+					<div className="contenedor">
+						<ul className="bg-white">
+							{productos.map(producto => (
+								<DetallesProducto 
+									key={producto.id}
+									producto={producto}
+								/>
+							))}
+						</ul>
+					</div>
+				</div>
+			</Layout>
+		</div>
+	);
+}
 
 export default Populares;
